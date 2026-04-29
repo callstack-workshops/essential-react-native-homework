2acc2cf03aad6a5ee9d5a3ecbcabd275f5be62f5
Nikita Kozlov <nikita.kozlov@getyourguide.com>
2026-04-29 15:05:48 +0200

    made navigation appear

---
+++ mobile/components/AddLottery.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function AddLottery() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Add Lottery</Text>
+    </View>
+  );
+} 
