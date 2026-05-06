d2d963dfb892b3b4f494594355d2931ed58009e1
Victor Zavala <victor.zavala@getyourguide.com>
2026-05-06 23:40:10 +0200

    Add RootStack to the app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './screens/Home';
+import AddLottery from './screens/AddLottery';
+import type { RootStackParamList } from './navigationTypes';
 
-export default function App() {
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="AddLottery" component={AddLottery} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
